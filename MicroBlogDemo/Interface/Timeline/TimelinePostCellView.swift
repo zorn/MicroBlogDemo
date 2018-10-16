429c893394bccab21e01d58bb613a926591d592c
Mike Zornek <zorn@zornlabs.com>
2018-10-16 15:28:20 -0400

    TextView instead of TextField.

--- MicroBlogDemo/Interface/Timeline/TimelinePostCellView.swift
+++ MicroBlogDemo/Interface/Timeline/TimelinePostCellView.swift
@@ -5,13 +5,13 @@ class TimelinePostCellView: NSView {
     static let cellIdentifier = NSUserInterfaceItemIdentifier(rawValue: "TimelinePostCellView")
     
     @IBOutlet var nameTextField: NSTextField?
-    @IBOutlet var contentTextField: NSTextField?
+    @IBOutlet var contentTextView: NSTextView?
     @IBOutlet var dateTextField: NSTextField?
     
     var content: JSONFeed.Item? {
         didSet {
             nameTextField?.stringValue = content?.author?.name ?? "NAME"
-            contentTextField?.stringValue = content?.contentHTML ?? ""
+            contentTextView?.string = content?.contentHTML ?? ""
             
             if let date = content?.dateModified {
                 let dateFormatter = DateFormatter()
